8535465befc59e6cd7e228c7004b7eeb1889af66
Adrian Osmond <a.c.osmond@gmail.com>
2024-12-06 19:55:00 +0100

    Use safe-area-inset-bottom for bottom padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-slate-100 text-slate-900 p-4 text-center">
+      <body className="bg-slate-100 text-slate-900 p-4 pb-[max(1rem,env(safe-area-inset-bottom))] text-center">
         <h1 className="text-xl md:text-3xl font-bold mb-2 font-mono">
           Week planner
         </h1>
